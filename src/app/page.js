3dc13f1f3edb66636575e1fe34a9f3bfb6de6b4f
ADMIN\HPVictus <thienphuc1052004@gmail.com>
2025-11-03 17:39:39 +0700

    change music

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
   if (isMobile) {
     return (
       <div className="w-full h-screen flex items-center justify-center bg-[var(--background)] text-white text-xl p-4 text-center">
-        This site is not accessible on mobile devices. Please use a desktop browser. You're still able use the AI features.
+        This site is not accessible on mobile devices. Please use a desktop browser. Youre still able use the AI features.
       </div>
     );
   }
--- src/app/pages/Page1.js
+++ src/app/pages/Page1.js
@@ -223,11 +223,11 @@ export default function Page1() {
         playerRef.current = new window.YT.Player('youtube-player', {
           height: '0',
           width: '0',
-          videoId: 'NaJz1PdbjCM',
+          videoId: 'B_Cj6sV0dh0',
           playerVars: {
             autoplay: 1,
             loop: 1,
-            playlist: 'NaJz1PdbjCM,cYmD3BMv46o,Vwnp-2T3VFg,pL35m337Qa4',
+            playlist: 'B_Cj6sV0dh0,cYmD3BMv46o,Vwnp-2T3VFg,pL35m337Qa4',
             controls: 0,
             showinfo: 0,
             modestbranding: 1,
